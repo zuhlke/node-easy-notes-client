415a9adfb24f446f4a382f523259f3176bb3f117
Immo Hüneke <immo.huneke@zuhlke.com>
2018-08-09 14:42:02 +0100

    Different port number

--- scripts/client_setup.sh
+++ scripts/client_setup.sh
@@ -2,10 +2,10 @@
 
 cd "${0%/*}"
 . common_config.sh
-docker images --format '{{.Repository}}:{{.Tag}}' | grep easy-notes-client | xargs docker rmi
+docker images --format '{{.Repository}}:{{.Tag}}' | grep easy-notes-client | xargs docker rmi -f
 (cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v1 .)
 gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v1
-kubectl run easy-notes-client --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v1 --port=8080
+kubectl run easy-notes-client --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v1 --port=8081
 kubectl get deployments
 kubectl get pods
 kubectl cluster-info
