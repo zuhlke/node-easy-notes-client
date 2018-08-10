50462c24bf571b5cbbc6c3bac0c37db949a869fb
iris-yue-wang <irisluv@gmail.com>
2018-08-10 18:15:08 +0100

    refactor: formatted

--- app/controllers/note.controller.js
+++ app/controllers/note.controller.js
@@ -1,4 +1,3 @@
-const request = require('request-promise');
 const service = require('../services/note.service.js');
 
 exports.create = (req, res) => {
@@ -8,33 +7,36 @@ exports.create = (req, res) => {
         });
     }
 
-    service.create('http://easy-notes-app:8080/notes', req.body).then(data => {
-        res.send(data);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while create note."
+    service.create('http://easy-notes-app:8080/notes', req.body)
+        .then(data => {
+            res.send({note: data});
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while create note."
+            });
         });
-    });
 };
 
 exports.findAll = (req, res) => {
-    service.findAll('http://easy-notes-app:8080/notes').then(notes => {
-        res.send(notes);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
+    service.findAll('http://easy-notes-app:8080/notes')
+        .then(notes => {
+            res.send({notes: notes});
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving notes."
+            });
         });
-    });
 };
 
 exports.findOne = (req, res) => {
-    service.findOne('http://easy-notes-app:8080/notes/' + req.params.noteId).then(note => {
+    service.findOne('http://easy-notes-app:8080/notes/' + req.params.noteId)
+        .then(note => {
             if (!note) {
                 return res.status(404).send({
                     message: "Note not found with id " + req.params.noteId
                 });
             }
-            res.send(note);
+            res.send({note: note});
         }).catch(err => {
         return res.status(500).send({
             message: err.message
@@ -49,32 +51,35 @@ exports.update = (req, res) => {
         });
     }
 
-    service.update('http://easy-notes-app:8080/notes/' + req.params.noteId, req.body).then(note => {
-        if (!note) {
-            return res.status(404).send({
-                message: "Note not found with id " + req.params.noteId
+    service.update('http://easy-notes-app:8080/notes/' + req.params.noteId, req.body)
+        .then(note => {
+            if (!note) {
+                return res.status(404).send({
+                    message: "Note not found with id " + req.params.noteId
+                });
+            }
+            res.send({note: note});
+        }).catch(err => {
+            return res.status(500).send({
+                message: err.message
             });
-        }
-        res.send(note);
-    }).catch(err => {
-        return res.status(500).send({
-            message: err.message
         });
-    });
 };
 
 exports.deleteAll = (req, res) => {
-    service.deleteAll('http://easy-notes-app:8080/notes').then(response => {
-            res.send({message: "All notes deleted successfully."});
-        }).catch(err => {
-        return res.status(500).send({
-            message: err.message
+    service.deleteAll('http://easy-notes-app:8080/notes')
+        .then(
+            res.send({message: "All notes deleted successfully."}))
+        .catch(err => {
+            return res.status(500).send({
+                message: err.message
+            });
         });
-    });
 };
 
 exports.delete = (req, res) => {
-    service.delete('http://easy-notes-app:8080/notes/' + req.params.noteId).then(response => {
+    service.delete('http://easy-notes-app:8080/notes/' + req.params.noteId)
+        .then(response => {
             if (!response) {
                 return res.status(404).send({
                     message: "Note not found with id " + req.params.noteId
