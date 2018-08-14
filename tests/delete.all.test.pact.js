06675bae8ea80f01b6803d2923ecfe689d172ddf
Immo Hüneke <immo.huneke@zuhlke.com>
2018-08-14 16:14:08 +0100

    Refactored delete-all test

--- tests/delete.all.test.pact.js
+++ tests/delete.all.test.pact.js
@@ -3,43 +3,55 @@ const url = require('../jest.config.js').testURL;
 const services = require('../app/services/note.service.js');
 const deleteAll = services.noteService(url + '/notes').deleteAll;
 
-describe('The API', () => {
+const EXPECTED_BODY = {
+    message: "All notes deleted successfully."
+};
 
-    // Copy this block once per interaction under test
+function setExpectations(state, subscript) {
+    const interaction = {
+        state: state,
+        uponReceiving: 'a delete request to delete all notes',
+        withRequest: {
+            method: 'DELETE',
+            path: '/notes',
+            headers: {
+                Accept: 'application/json'
+            }
+        },
+        willRespondWith: {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            },
+            body: EXPECTED_BODY
+        }
+    };
+    return provider.addInteraction(interaction);
+}
+
+function doTheTest(state, subscript, done) {
+    return setExpectations(state, subscript).then(() => {
+        return deleteAll();
+    }).then(response => {
+        expect(response).toEqual(EXPECTED_BODY);
+    }).then(done)
+    .catch(done);
+}
+
+describe('The Delete API', () => {
+
     describe('Delete all notes when a delete request is sent to /notes without a node id', () => {
-        // given...
-        const EXPECTED_BODY = {
-            message: "All notes deleted successfully."
-        };
-        beforeEach(() => {
-            const interaction = {
-                uponReceiving: 'a delete request to delete all notes',
-                withRequest: {
-                    method: 'DELETE',
-                    path: '/notes',
-                    headers: {
-                        Accept: 'application/json'
-                    }
-                },
-                willRespondWith: {
-                    status: 200,
-                    headers: {
-                        'Content-Type': 'application/json; charset=utf-8'
-                    },
-                    body: EXPECTED_BODY
-                }
-            };
-            return provider.addInteraction(interaction);
+
+        it('will delete no notes', done => {
+            doTheTest('no notes', 0, done);
         });
 
-        // when...
-        it('Delete the note', done => {
-            // then...
-            deleteAll()
-                .then(response => {
-                    expect(response).toEqual(EXPECTED_BODY);
-                })
-                .then(done);
+        it('will delete first note', done => {
+            doTheTest('first note', 1, done);
+        });
+
+        it('will delete two notes', done => {
+            doTheTest('two notes', 2, done);
         });
     });
 });
