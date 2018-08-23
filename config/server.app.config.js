0610382783ad3f2e981a70e3ead51205f0671c53
iris-yue-wang <irisluv@gmail.com>
2018-08-23 16:13:43 +0100

    modify: client to connect to springb app

--- config/server.app.config.js
+++ config/server.app.config.js
@@ -2,7 +2,7 @@ const os = require('os');
 
 module.exports = (os.platform() == 'darwin') ?
 {
-    url: 'http://localhost:8082/notes'
+    url: 'http://localhost:8083/notes'
 } : {
-    url: 'http://easy-notes-app:8082/notes'
+    url: 'http://springb-easy-notes-app:8083/notes'
 };
--- scripts/client_update.sh
+++ scripts/client_update.sh
@@ -2,7 +2,7 @@
 
 cd "${0%/*}"
 
-(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v4 .)
-gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v4
+(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5 .)
+gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5
 kubectl edit deployment easy-notes-client
 kubectl get deployments
