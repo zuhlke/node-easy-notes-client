53814a66cbb4174556debbce55ae3ef0cc3715b3
Immo Hüneke <immo.huneke@zuhlke.com>
2018-08-28 16:10:26 +0100

    Added start of a Web UI

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true}));
-
+app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes client. Take notes quickly. Organise and keep track of all your notes."});
+    res.render('index', {
+        title: 'Easy Notes',
+        message: 'Welcome to EasyNotes client. Take notes quickly. Organise and keep track of all your notes.'
+    });
 });
 
 app.listen(8081, () => {
@@ -16,3 +27,4 @@ app.listen(8081, () => {
 });
 
 require('./app/routes/note.routes.js')(app);
+
