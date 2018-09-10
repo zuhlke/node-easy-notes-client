646779ccad648add740d375715388d0002433415
iris-yue-wang <irisluv@gmail.com>
2018-09-10 10:50:54 +0100

    modify: use kubectl command to set new image for rolling update

--- scripts/client_setup.sh
+++ scripts/client_setup.sh
@@ -3,9 +3,9 @@
 cd "${0%/*}"
 . common_config.sh
 docker images --format '{{.Repository}}:{{.Tag}}' | grep easy-notes-client | xargs docker rmi -f
-(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v3 .)
-gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v3
-kubectl run easy-notes-client --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v3 --port=8081
+(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v6 .)
+gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v6
+kubectl run easy-notes-client --image=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v6 --port=8081
 kubectl get deployments
 kubectl get pods
 kubectl cluster-info
--- scripts/client_update.sh
+++ scripts/client_update.sh
@@ -2,7 +2,8 @@
 
 cd "${0%/*}"
 
-(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5 .)
-gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5
-kubectl edit deployment easy-notes-client
+(cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v7 .)
+gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v7
+#kubectl edit deployment easy-notes-client
+kubectl set image deployment/easy-notes-client easy-notes-client=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v7
 kubectl get deployments
